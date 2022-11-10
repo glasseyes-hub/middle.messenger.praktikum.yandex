9a846b065022559b4f9f7a47119bef4cca754e88
Alexey Polishchuk <a.polishchuk@ze.studio>
2022-11-10 10:08:51 +0300

    fixed showing data

--- src/pages/profile/edit/data/index.js
+++ src/pages/profile/edit/data/index.js
@@ -3,7 +3,12 @@ import template from './index.hbs'
 
 export default function render(params = {}) {
     return template({
-        password: '1234567890', 
+        email: "pochta@yandex.ru", 
+        login: "ivanivanov", 
+        first_name: "Иван", 
+        second_name: "Иванов", 
+        display_name: 'Иван', 
+        phone: '+7 (909) 967 30 30', 
         ...params
     })
 }
