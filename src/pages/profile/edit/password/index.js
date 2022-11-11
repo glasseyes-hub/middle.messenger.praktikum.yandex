a21439760aa51e50e798f23999ab6e69e59f9f55
Alexey Polishchuk <a.polishchuk@ze.studio>
2022-11-11 09:04:39 +0300

    refactoring rendering pages

--- src/pages/profile/edit/password/index.js
+++ src/pages/profile/edit/password/index.js
@@ -1,8 +1,9 @@
 import template from './index.hbs'
+import { render } from '../../../../utils/render'
 
 
-export default function render(params = {}) {
-    return template({
+export default function (params = {}) {
+    render(template, {
         password: '1234567890', 
         ...params
     })
