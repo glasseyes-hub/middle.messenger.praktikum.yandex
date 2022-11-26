706d4286d058b26b499559d44f7f91a21253e0c5
Alexey Polishchuk <a.polishchuk@ze.studio>
2022-11-26 15:46:07 +0300

    added registerComponent helper

---
+++ src/utils/registerComponent.ts
@@ -0,0 +1,33 @@
+import Block from './Block';
+import * as Handlebars from 'handlebars';
+
+export function registerComponent(name: string, Component: typeof Block) {
+  Handlebars.registerHelper(name, function ( { hash, data, fn }: Handlebars.HelperOptions) {
+    // console.log('123')
+    if (!data.root.children) {
+      data.root.children = {};
+    }
+
+    if (!data.root.refs) {
+      data.root.refs = {};
+    }
+
+
+    console.log(hash)
+
+    const component = new Component(hash);
+
+    data.root.children[component.id] = component
+
+
+
+    if (hash.ref) {
+      data.root.refs[hash.ref] = component;
+    }
+
+
+    const contents: string = fn ? fn(this) : '';
+
+    return `<div data-id="${component.id}">${contents}</div>`;
+  })
+}
